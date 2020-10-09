5e897735019d847d1b9f9d16c3a961e81b4659f5
beamun <71042801+beamun@users.noreply.github.com>
2020-10-09 18:46:29 +0700

    Add files via upload

---
+++ atm-spring-code-config/src/main/java/th/ac/kmitl/Main.java
@@ -0,0 +1,17 @@
+package th.ac.kmitl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+
+        ATM atm = context.getBean(ATM.class);
+
+        String name = atm.validateCustomer(2, 2345);
+        System.out.println( name + " has " + atm.getBalance());
+    }
+}
